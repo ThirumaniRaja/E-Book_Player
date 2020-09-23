797d8669fe14c4ac9743b3a2f921c7c8496c2e8a
Thirumani <thirumaniraja143@gmail.com>
2020-09-23 20:50:10 +0530

    first commit

---
+++ apps/ebook-player/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ebook-player-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ebook-player';
+}
